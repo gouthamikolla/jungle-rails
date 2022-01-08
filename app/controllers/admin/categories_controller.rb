7d2ab702e5d4edc4a4463ccbe662256001d6332e
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2022-01-07 23:38:26 -0500

    update changes for category controller and index.html

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,8 +1,12 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV['ADMIN_NAME'], :password => ENV['ADMIN_PASSWORD']
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
